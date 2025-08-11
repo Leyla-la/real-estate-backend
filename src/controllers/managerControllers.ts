48c010cff59f9046ac121e69327cb416ddbebd1e
Leyla-la <tranthigiangpltn@gmail.com>
2025-08-11 10:58:05 +0700

    auth and RBAC

---
+++ src/controllers/managerControllers.ts
@@ -0,0 +1,41 @@
+import { PrismaClient } from "@prisma/client/extension";
+import { Request, Response } from "express";
+
+const prisma = new PrismaClient();
+
+export const getManager = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { cognitoId } = req.params;
+        const manager = await prisma.manager.findUnique({
+            where: { cognitoId },
+        });
+
+        if (manager) {
+            res.json(manager)
+        } else {
+            res.status(404).json({ message: "manager not found" })
+        }
+    } catch (error: any) {
+        res.status(500).json({ message: `Error retrieving manager: ${error.message}` });
+    }
+};
+
+export const createManager = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { cognitoId, name, email, phoneNumber } = req.body;
+    const manager = await prisma.manager.create({
+      data: {
+        cognitoId,
+        name,
+        email,
+        phoneNumber,
+      },
+    });
+
+      res.status(200).json(manager);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error creating manager: ${error.message}` });
+  }
+};
