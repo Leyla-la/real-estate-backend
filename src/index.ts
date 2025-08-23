e08df598dd6d210480e6c8fb34cc77775debd495
Leyla-la <tranthigiangpltn@gmail.com>
2025-08-23 10:28:33 +0700

    fix wrong property route name, undefined location

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
-import { authMiddleware } from "./middleware/authMiddleware";
 import tenantRoutes from "./routes/tenantRoutes";
 import managerRoutes from "./routes/managerRoutes";
 import propertyRoutes from "./routes/propertyRoutes";
 import leaseRoutes from "./routes/leaseRoutes";
 import applicationRoutes from "./routes/applicationRoutes";
+import { authMiddleware } from "./middleware/authMiddleware";
 
 
 
--- src/routes/propertyRoutes.ts
+++ src/routes/propertyRoutes.ts
@@ -1,24 +1,24 @@
 import express from "express";
 import {
   getProperties,
-  createProperty,
   getProperty,
+  createProperty,
 } from "../controllers/propertyControllers";
-import { authMiddleware } from "../middleware/authMiddleware";
 import multer from "multer";
+import { authMiddleware } from "../middleware/authMiddleware";
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
 const router = express.Router();
 
-router.get("/", getProperty);
-router.get("/:id", getProperties);
+router.get("/", getProperties);
+router.get("/:id", getProperty);
 router.post(
-    "/",
-    authMiddleware(["manager"]),
-    upload.array("photos"),
-    createProperty
-); 
+  "/",
+  authMiddleware(["manager"]),
+  upload.array("photos"),
+  createProperty
+);
 
 export default router;
